eeab3b621f086d44c6a038f7445bac093db4138a
Rian <63377157+rharshaw@users.noreply.github.com>
2022-05-28 14:54:48 -0400

    Initial Commit

---
+++ PrimeAuto/PrimeAutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeAutoApp.swift
+//  PrimeAuto
+//
+//  Created by Rian Harshaw on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PrimeAutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
